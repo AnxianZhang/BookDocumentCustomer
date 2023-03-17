94730e2804cd0761dd8ad07d7c79916295ba50c2
linxingt <xingtonglin011016@gmail.com>
2023-03-17 20:54:59 +0100

    depart les 3 service et appClient

--- src/CustomerApp.java
+++ src/CustomerApp.java
@@ -6,23 +6,52 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class CustomerApp {
+    private static int RESERVATION = 3000;
+    private static int EMPRUNT = 4000;
+    private static int RETOURS = 5000;
+    private static String HOST = "localhost";
+
     public static void main(String[] args) throws IOException {
-        // TODO Auto-generated method stub
-        Socket clientSocket = new Socket("localhost", 1234);
-        InputStreamReader isReader = new InputStreamReader(clientSocket.getInputStream());
-        BufferedReader reader = new BufferedReader(isReader);
-        PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
-        Scanner scanner = new Scanner(System.in);
-        while(true)
-        {
-            String send = scanner.nextLine();
-            writer.println(send);
-            System.out.println(reader.readLine());
-//			String echo = reader.readLine();
-//			System.out.println("Server echo: " + echo);
-            if(send.equals("end")) break;
+        BufferedReader clavier = new BufferedReader(new InputStreamReader(System.in));
+
+        Socket socket = null;
+        try {
+            socket = new Socket(HOST, EMPRUNT);
+            BufferedReader sin = new BufferedReader (new InputStreamReader(socket.getInputStream ( )));
+            PrintWriter sout = new PrintWriter (socket.getOutputStream ( ), true);
+            // Informe l'utilisateur de la connection
+            System.out.println("Connecté au serveur " + socket.getInetAddress() + ":"+ socket.getPort());
+
+            String line;
+            line = sin.readLine(); // 1ère question
+            System.out.println(line);
+            // prompt d'invite à la saisie
+            System.out.print("->");
+            line = clavier.readLine();
+            sout.println(line);
+            line = sin.readLine(); // 2ème question//ya pbm pour retourService qui a 1 seul qst
+            System.out.println(line);
+            // prompt d'invite à la saisie
+            System.out.print("->");
+            line = clavier.readLine();
+            sout.println(line);
+
+            System.out.println(sin.readLine()); // réponse finale
+
+            socket.close();
         }
-        System.out.println("Client socket colsed !");
-        clientSocket.close();
+        catch (IOException e) { System.err.println("Fin du service"); }
+        // Refermer dans tous les cas la socket
+        try { if (socket != null) socket.close(); }
+        catch (IOException e2) { ; }
+    }
+
+    private static boolean isNumeric(String string) {
+        try {
+            Integer.parseInt(string);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
